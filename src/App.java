ba65ed398a62b60246af6eeb4c430d962ad8fbff
Jonnathan2124 <jxyanza17@gmail.com>
2024-10-15 00:09:14 -0500

    Aplicacion

--- src/App.java
+++ src/App.java
@@ -1,12 +1,113 @@
+
+import java.util.Scanner;
+
 public class App {
 
+    static int[] arreglo;
+
     public static void main(String[] args) throws Exception {
-
         
-
-        Menu menu = new Menu();
-
-        menu.inicio();
+        App app = new App();
+        app.inicio();
+    }
+    public void inicio() {
         
+        Metodo ordenamiento = new Metodo();
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        System.out.println("MENÚ:");
+
+        do {
+            System.out.println("Menu Principal");
+            System.out.println("1. Ingrese un arreglo");
+            System.out.println("2. Ordenar Arreglo");
+            System.out.println("0. Salir");
+            option = scanner.nextInt();
+
+            switch(option) {
+                case 1:
+                    ingresarArreglo(scanner);
+                    break;
+                case 2:
+                    if(arreglo != null) {
+                        ordenarArreglo(scanner, ordenamiento);
+                    } else {
+                        System.out.println("Primero debe ingresar un arreglo.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Cerrando...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        } while(option != 0);
+        
+        scanner.close();
+    }
+
+    public static void ingresarArreglo(Scanner scanner) {
+        System.out.println("Ingrese el tamaño del arreglo: ");
+        int tamanio = scanner.nextInt();
+        arreglo = new int[tamanio];
+
+        for(int i = 0; i < tamanio; i++) {
+            System.out.println("Ingrese el valor para el elemento " + (i+1) + ":");
+            arreglo[i] = scanner.nextInt();
+        }
+    }
+
+    public static void ordenarArreglo(Scanner scanner, Metodo ordenamiento) {
+        int opcion;
+
+        do {
+            System.out.println("Escoja un metodo de ordenamiento");
+            System.out.println("1. Metodo Burbuja");
+            System.out.println("2. Metodo Seleccion");
+            System.out.println("3. Metodo Insercion");
+            System.out.println("4. Metodo Burbuja Mejorado");
+            System.out.println("0. Regresar al menu principal");
+            opcion = scanner.nextInt();
+
+            if (opcion >= 1 && opcion <= 4) {
+                
+                System.out.println("Escoja el tipo de orden:");
+                System.out.println("1. Ascendente");
+                System.out.println("2. Descendente");
+                int orden = scanner.nextInt();
+
+                
+                System.out.println("Quiere ver cada paso?");
+                System.out.println("1. Si");
+                System.out.println("2. No");
+                int mostrarPasos = scanner.nextInt();
+
+                
+                switch(opcion) {
+                    case 1:
+                        System.out.println("Metodo Burbuja:");
+                        ordenamiento.burbujaNormal(arreglo, orden == 1, mostrarPasos == 1);
+                        break;
+                    case 2:
+                        System.out.println("Metodo de Seleccion:");
+                        ordenamiento.seleccion(arreglo, orden == 1, mostrarPasos == 1);
+                        break;
+                    case 3:
+                        System.out.println("Metodo de Insercion:");
+                        ordenamiento.insercion(arreglo, orden == 1, mostrarPasos == 1);
+                        break;
+                    case 4:
+                        System.out.println("Metodo Burbuja Mejorado:");
+                        ordenamiento.burbujaMejorado(arreglo, orden == 1, mostrarPasos == 1);
+                        break;
+                }
+            } else if (opcion != 0) {
+                System.out.println("Opción no válida.");
+            }
+
+        } while(opcion != 0);
     }
 }
+
--- src/Metodo.java
+++ src/Metodo.java
@@ -1,24 +1,105 @@
 public class Metodo{
     
 
-     public int[] burbujaNormal(int[] arreglo) {
-        int tamanio = arreglo.length;
-        for (int i = 0; i < tamanio; i++) {
-             for (int j = i + 1; j < tamanio; j++) {
-                if (arreglo[i] > arreglo[j]) {
-                     int aux = arreglo[i];
-                    arreglo[i] = arreglo[j];
-                    arreglo[j] = aux;
+     public void burbujaNormal(int[] arreglo, boolean ascendente, boolean mostrarPasos) {
+        int n = arreglo.length;
+        for (int i = 0; i < n -1; i++) {
+             for (int j = 0 + 1; j < n-i-1; j++) {
+                boolean condicion = ascendente ? arreglo[j] > arreglo[j+1] : arreglo[j] < arreglo[j=1];
+                if (condicion) {
+                     int aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;
                     }
+                if (mostrarPasos){
+                    imprimirArreglo(arreglo);
+
+                    }   
                 }
             }
-            return arreglo;
+            System.out.println("Arreglo ordenado");
+            imprimirArreglo(arreglo);
         }
-    
-     public void burbujaOrdenado(int[] arreglo) {
-        for (int elemento : arreglo) {
-             System.out.print(elemento + " ");
+
+        public void seleccion(int[] arreglo, boolean ascendente, boolean mostraPasos){
+            int n = arreglo.length;
+            
+            for(int i = 0; i< n-1; i++){
+                int indiceExtremo = i;
+                for(int j = i+1; j<n; j++){
+                    boolean condicion = ascendente ? arreglo[j] < arreglo[indiceExtremo] : arreglo[j] > arreglo[indiceExtremo];
+
+                    if(condicion){
+                        indiceExtremo = j;
+
+                    }
+
+                }
+                int temp = arreglo[indiceExtremo];
+                arreglo[indiceExtremo] = arreglo[i];
+                arreglo[i] = temp;
+
+                if(mostraPasos){
+                    imprimirArreglo(arreglo);
+                }
             }
+            System.out.println("Arreglo Ordenado:");
+            imprimirArreglo(arreglo);
+        }
+
+        public void insercion (int[] arreglo, boolean ascendente, boolean mostrarPasos){
+            int n = arreglo.length;
+
+            for(int i = 1; i < n; ++i){
+                int clave = arreglo[i];
+                int j = i -1;
+
+                 while(j >= 0 && (ascendente ? arreglo[j] > clave : arreglo[j] < clave)){
+                    arreglo[j =1] = arreglo[j];
+                    j = j-1;                    
+                 }
+                 arreglo[j + 1] = clave;
+                 if(mostrarPasos){
+                    imprimirArreglo(arreglo);
+                 }
+            }
+            System.out.println("Arreglo ordenado");
+            imprimirArreglo(arreglo);
+        }
+
+        public void burbujaMejorado(int[] arreglo, boolean ascendente, boolean mostrarPasos){
+            int n = arreglo.length;
+            boolean intercambio;
+
+            for(int i = 0; i < n-1; i++){
+                intercambio = false;
+                for(int j = 0; j < n-i-1; j++){
+                    boolean condicion = ascendente ? arreglo[j] > arreglo[j+1] : arreglo[j] < arreglo[j+1];
+                    
+                    if(condicion){
+                        int temp = arreglo[j];
+                        arreglo[j] = arreglo[j+1];
+                        arreglo[j+1] = temp;
+                        intercambio = true; 
+                    }
+
+                    if(mostrarPasos){
+                        imprimirArreglo(arreglo);
+                    }
+                }
+                if(!intercambio)
+                break;
+            }
+            System.out.println("Arreglo ordenado: ");
+            imprimirArreglo(arreglo);
+        }
+
+        public void imprimirArreglo(int[] arreglo){
+            for(int i : arreglo){
+                System.out.println(i + " ");
+
+            }
+            System.out.println();
         }
 
  }
